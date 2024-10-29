eeeee9c7aac227ba311bafdcd3c30c22fef6131e
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2024-10-30 00:08:10 +0100

    Added zone

--- src/tc001v4.2.py
+++ src/tc001v4.2.py
@@ -8,28 +8,6 @@ from http.client import responses
 
 from numpy.core.multiarray import unravel_index
 
-from src.gui import draw_dot
-
-print('Les Wright 21 June 2023')
-print('https://youtube.com/leslaboratory')
-print('A Python program to read, parse and display thermal data from the Topdon TC001 Thermal camera!')
-print('')
-print('Tested on Debian all features are working correctly')
-print('This will work on the Pi However a number of workarounds are implemented!')
-print('Seemingly there are bugs in the compiled version of cv2 that ships with the Pi!')
-print('')
-print('Key Bindings:')
-print('')
-print('a z: Increase/Decrease Blur')
-print('s x: Floating High and Low Temp Label Threshold')
-print('d c: Change Interpolated scale Note: This will not change the window size on the Pi')
-print('f v: Contrast')
-print('q w: Fullscreen Windowed (note going back to windowed does not seem to work on the Pi!)')
-print('r t: Record and Stop')
-print('p : Snapshot')
-print('m : Cycle through ColorMaps')
-print('h : Toggle HUD')
-
 import cv2
 import gui
 import numpy as np
@@ -95,8 +73,7 @@ colormaps = [('Jet', cv2.COLORMAP_JET),
 			 ('Spring', cv2.COLORMAP_SPRING),
 			 ('Autumn', cv2.COLORMAP_AUTUMN),
 			 ('Viridis', cv2.COLORMAP_VIRIDIS),
-			 ('Parula', cv2.COLORMAP_PARULA),
-			 ('Inv Rainbow', cv2.COLORMAP_RAINBOW)]
+			 ('Parula', cv2.COLORMAP_PARULA)]
 
 def rec():
 	now = time.strftime("%Y%m%d--%H%M%S")
@@ -111,27 +88,27 @@ def snapshot(heatmap):
 	cv2.imwrite("TC001"+now+".png", heatmap)
 	return snaptime
 
-def findHighest():
-	linear_max = th_data[...].argmax()
-	row, col = unravel_index(linear_max, (192, 256))
-	return (col, row, th_data[row, col])
+def findHighest(data):
+	linear_max = data[...].argmax()
+	row, col = unravel_index(linear_max, data.shape)
+	return (col, row, data[row, col])
 
-def findLowest():
-	linear_max = th_data[...].argmin()
-	row, col = unravel_index(linear_max, (192, 256))
-	return (col, row, th_data[row, col])
+def findLowest(data):
+	linear_max = data[...].argmin()
+	row, col = unravel_index(linear_max, data.shape)
+	return (col, row, data[row, col])
 
-def findAverage():
-	return round(th_data[...].mean(), 2)
-
+def findAverage(data):
+	return round(data[...].mean(), 2)
 
 def applyColorMap(colormap_index):
 	colormap_title, colormap = colormaps[colormap_index]
 	heatmap = cv2.applyColorMap(bgr, colormap)
 	return colormap_title, heatmap
 
-def writeText(text, x, y):
-	cv2.putText(image, text, (x, y), font, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+def getBounds(bottom, top, left, right):
+	return th_data[bottom:top, left:right]
+
 
 # Converting the raw values to celsius
 # https://www.eevblog.com/forum/thermal-imaging/infiray-and-their-p2-pro-discussion/200/
@@ -145,20 +122,20 @@ def writeText(text, x, y):
 def convertRawToCelcius(raw_temp):
 	return np.round(((raw_th_data[..., 1].astype(np.uint16) << 8) + raw_th_data[..., 0].astype(np.uint16)) / 64 - 273.15, 2)
 
-while(cap.isOpened()):
+while cap.isOpened():
 	# Capture frame-by-frame
 	ret, frame = cap.read()
 
-	if ret == True:
+	if ret:
 		im_data,raw_th_data = np.array_split(frame, 2)
 		th_data = convertRawToCelcius(raw_th_data)
 
 		temp = th_data[96, 128]
 
-		mrow, mcol, maxtemp = findHighest()
-		lrow, lcol, mintemp = findLowest()
+		mrow, mcol, maxtemp = findHighest(th_data)
+		lrow, lcol, mintemp = findLowest(th_data)
 
-		avg_temp = findAverage()
+		avg_temp = findAverage(th_data)
 
 		# Convert the real image to RGB
 		bgr = cv2.cvtColor(im_data, cv2.COLOR_YUV2BGR_YUYV)
@@ -175,78 +152,29 @@ while(cap.isOpened()):
 		#apply colormap
 		cmapText, image = applyColorMap(colormap_index)
 
-		if(colormap_index == 10):
-			image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-		gui.draw_crosshair(image, temp, scaled_width, scaled_height)
-
-		gui.draw_menu(hud, image, avg_temp, threshold, cmapText, rad, scale, alpha, snaptime, recording, elapsed)
-
+		#gui.draw_crosshair(image, temp, scaled_width, scaled_height)
+		#gui.draw_menu(hud, image, avg_temp, threshold, cmapText, rad, scale, alpha, snaptime, recording, elapsed)
 		gui.draw_dot(image, mrow, mcol, scale, (0, 0, 255), maxtemp)
-
 		gui.draw_dot(image, lrow, lcol, scale, (255, 0, 0), mintemp)
+
+		zone1 = getBounds(0, 64, 0, 64)
+
+		z_l_row, z_l_col, z_min_temp = findLowest(zone1)
+		z_m_row, z_m_col, z_max_temp = findHighest(zone1)
+
+		gui.draw_dot(image, z_l_row, z_l_col, scale, (0, 255, 0), z_min_temp)
+		gui.draw_dot(image, z_m_row, z_m_col, scale, (0, 255, 0), z_max_temp)
 
 		#display image
 		cv2.imshow('Thermal', image)
 
-		if recording == True:
-			elapsed = (time.time() - start)
-			elapsed = time.strftime("%H:%M:%S", time.gmtime(elapsed))
-			videoOut.write(image)
 		
 		keyPress = cv2.waitKey(1)
-		if keyPress == ord('a'): #Increase blur radius
-			rad += 1
-		if keyPress == ord('z'): #Decrease blur radius
-			rad -= 1
-			if rad <= 0:
-				rad = 0
-
-		if keyPress == ord('s'): #Increase threshold
-			threshold += 1
-		if keyPress == ord('x'): #Decrease threashold
-			threshold -= 1
-			if threshold <= 0:
-				threshold = 0
-
-		if keyPress == ord('d'): #Increase scale
-			scale += 1
-			if scale >=5:
-				scale = 5
-			scaled_width = width * scale
-			scaled_height = height * scale
-			if dispFullscreen == False and isPi == False:
-				cv2.resizeWindow('Thermal', scaled_width, scaled_height)
-		if keyPress == ord('c'): #Decrease scale
-			scale -= 1
-			if scale <= 1:
-				scale = 1
-			scaled_width = width * scale
-			scaled_height = height * scale
-			if dispFullscreen == False and isPi == False:
-				cv2.resizeWindow('Thermal', scaled_width, scaled_height)
 
 		if keyPress == ord('q'): #enable fullscreen
 			dispFullscreen = True
 			cv2.namedWindow('Thermal',cv2.WND_PROP_FULLSCREEN)
 			cv2.setWindowProperty('Thermal',cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-		if keyPress == ord('w'): #disable fullscreen
-			dispFullscreen = False
-			cv2.namedWindow('Thermal',cv2.WINDOW_GUI_NORMAL)
-			cv2.setWindowProperty('Thermal',cv2.WND_PROP_AUTOSIZE,cv2.WINDOW_GUI_NORMAL)
-			cv2.resizeWindow('Thermal', scaled_width, scaled_height)
-
-		if keyPress == ord('f'): #contrast+
-			alpha += 0.1
-			alpha = round(alpha,1)#fix round error
-			if alpha >= 3.0:
-				alpha=3.0
-		if keyPress == ord('v'): #contrast-
-			alpha -= 0.1
-			alpha = round(alpha,1)#fix round error
-			if alpha<=0:
-				alpha = 0.0
-
 
 		if keyPress == ord('h'):
 			hud = not hud
@@ -256,19 +184,7 @@ while(cap.isOpened()):
 			if colormap_index == 11:
 				colormap_index = 0
 
-		if keyPress == ord('r') and recording == False: #r to start reording
-			videoOut = rec()
-			recording = True
-			start = time.time()
-		if keyPress == ord('t'): #f to finish reording
-			recording = False
-			elapsed = "00:00:00"
-
-		if keyPress == ord('p'): #f to finish reording
-			snaptime = snapshot(image)
-
 		if keyPress == ord('q'):
 			break
 			capture.release()
 			cv2.destroyAllWindows()
-		
